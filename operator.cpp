3cc1b28d672515b3cb7903834f9d491bb3e7d821
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-03 11:55:08 +0530

    operator

---
+++ operator.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int x = 5;
+    int y = 3;
+
+    cout << x + y << endl;
+    cout << x - y << endl;
+    cout << x * y << endl;
+    cout << x / y << endl;
+    cout << x % y << endl;
+
+    --x;
+    --y;
+    cout << x << endl;
+    cout << y;
+
+
+}
