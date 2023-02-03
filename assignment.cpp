133e7f61a3a7e40db7e5469ad0964534b3df3418
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-03 12:10:32 +0530

    assignment operators

---
+++ assignment.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using  namespace std;
+int main(){
+
+    int x =20;
+  // x=x+3
+     x += 3;
+    cout << x << endl;
+       x -= 3;
+    cout << x << endl;
+       x *= 3;
+    cout << x << endl;
+       x %= 3;
+    cout << x << endl;
+       x ^= 3;
+    cout << x << endl;
+    //    x &= 3;
+    // cout << x << endl;
+
+}
