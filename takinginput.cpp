33f746e7378b97c9f204c3d01c0b4a9ab73cfeda
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-03 11:33:27 +0530

    taking input from user

---
+++ takinginput.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int x, y;
+    int sum;
+
+    cout << "The first number is :";
+    cin >> x;
+
+    cout << "The second number is :";
+    cin >> y;
+
+    sum = x + y;
+
+    cout << "The sum is :" << sum << endl;
+    return 0;
+}
