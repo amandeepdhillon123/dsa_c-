f26a07e403f35de9e6800e98e5ee687271e170ce
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-03 12:48:10 +0530

    string

---
+++ string.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+
+// string name ="amandeep";
+// cout << name << endl ;
+// concatenation 
+/*
+string firstname = " aman" ;
+string  lastname = " Dhillon";
+// string  fullname = firstname + lastname ;
+string fullname= firstname.append(lastname); 
+ 
+ cout << fullname << endl ;
+
+  */
+
+//  number and string 
+
+// 1 string
+
+// string a="10";
+// string b="10";
+// cout << a+b << endl ;
+
+// error occured 
+
+// string a="hjbjh";
+// int b = 10;
+// cout << a + b << endl ;
+
+
+// string lenght 
+
+// string ram ="dfjkdfjkdfnkjsdfvnjsdnvsdkjvn";
+
+
+// cout << ram.size() << endl;
+// cout << ram.length() << endl;
+
+
+// access string 
+
+// string ram ="dfjkdfjkdfnkjsdfvnjsdnvsdkjvn";
+
+//  ram[0] = 'r';
+// cout << ram << endl;
+
+//  input tajkin string 
+
+string fullname ;
+cout << " enter the full name ";
+cin >> fullname;
+cout << "print the name " << fullname;
+
+
+
+
+
+
+
+
+// cout << ram[28] <<  endl;
+
+
+
+
+}
