f2ae365764c3936b42ccad3464083d3fc0292715
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-04 13:06:10 +0530

    reverse hybrid number

---
+++ pattern/p9.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  
+     cout << "put the value for half pyramid ";
+       int n;
+    cin >> n;
+
+   
+    for (int i = 0 ; i < n ; i++)
+
+    {
+        for (int j = 0; j < n-i ; j++)
+        {
+            cout << j+1 ;
+        }
+        cout << endl; 
+    }
+}
