8edf2ee5df13a114f9c74b808c71904ef9fb094b
amandeepdhillon123 <amandeep24397@gmail.com>
2023-02-10 07:05:52 +0530

    print a number in function

---
+++ function/p1.cpp
@@ -0,0 +1,19 @@
+
+// write a function print a number number 
+
+#include<iostream>
+
+using namespace std;
+
+void printNum(int num){
+
+cout << num;
+
+}
+
+int main(){
+    int a=5;
+   printNum(a);
+   return 0;
+
+}
