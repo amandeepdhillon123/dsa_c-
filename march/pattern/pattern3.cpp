dbd50a488fb2670be37176b79e35706896d279e3
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-14 17:26:35 +0530

    pattern 3

---
+++ march/pattern/pattern3.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int row;
+    cout << " Enter the row :";
+    cin >> row;
+
+    for(int i=0; i<row; i++)
+    {
+       for(int b=0; b<i+1; b++)
+       {
+        cout << "* ";
+       }
+         cout <<endl;
+    }
+  
+}
+
