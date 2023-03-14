b284e28fe2f16fd037546f6b285b13c0d17f5ffe
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-14 17:45:32 +0530

    pattern 6

---
+++ march/pattern/pattern6.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int row;
+    cout << " Enter the row :";
+    cin >> row;
+
+    for(int i=0; i<row; i++)
+    {
+       for(int b=0; b<row -(i); b++)
+       {
+        cout << (b+1) << " ";
+       }
+         cout <<endl;
+    }
+  
+}
