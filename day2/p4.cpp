e0529435229a4c937cc095a8d0713216c0988fa4
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-21 10:05:34 +0530

    pre decrement an dpost decrement operator

---
+++ day2/p4.cpp
@@ -0,0 +1,22 @@
+//  pre decrement and post decrement operator 
+
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    int a= 3;
+    int b = --a;
+
+    int c= a--;
+
+    cout << b << endl;
+
+
+    cout << c << endl;
+        cout << a << endl;
+
+
+
+}
