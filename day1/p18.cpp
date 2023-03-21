014c99aea832613079b77086f67900a02e98c967
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-21 08:24:27 +0530

    full nueric pyramid

---
+++ day1/p18.cpp
@@ -0,0 +1,37 @@
+// numeric full pyramid 
+
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout << " enter tha value of n: ";
+    cin >> n;
+
+    for( int row=0; row<n; row++)
+    {
+          for( int col=0; col<n-row-1; col++)
+          {
+            cout <<  " ";
+          }
+
+        // for number 
+   
+
+        for(int col =0; col < row+1; col =col+1)
+        {
+            cout << row + col + 1;
+           
+       }
+        // reverse couting 
+        int start = 2 * row;
+       for( int col=0; col <row; col = col+1)
+       {
+        cout << start ;
+        start = start -1 ;
+       }
+       cout << endl ;
+
+    }
+}
