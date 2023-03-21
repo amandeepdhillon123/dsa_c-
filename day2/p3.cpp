2a16963b30b14f5a66c7d7a84124e6fafc899aa1
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-21 10:02:01 +0530

    pre increment and post increment

---
+++ day2/p3.cpp
@@ -0,0 +1,21 @@
+//  pre increment and post decrement operator 
+
+#include<iostream>
+
+using namespace std;
+
+int main (){
+
+    int a= 3;
+    int b = ++a;
+
+    int c= a++;
+
+    cout << b << endl;
+
+    cout << a << endl;
+    cout << c << endl;
+
+
+
+}
