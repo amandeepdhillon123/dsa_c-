85550b744dffcda2cbc66be41ca388415b96f21c
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-21 09:39:01 +0530

    bitwise not operator

---
+++ day2/p1.cpp
@@ -0,0 +1,25 @@
+// bit wise operator 
+
+// And or not XOR 
+
+// every value shoutld be true  in and case 
+
+//  one value should be true in or case 
+
+// evry value should be oppsoye in not case ! 
+
+// same value give 0 and differt give 1
+
+// left and shift operator 
+
+//  not operator 
+#include<iostream>
+
+using namespace std ;
+int main(){
+
+    int a =6;
+    int b =7;
+
+    cout << (~ a);
+}
