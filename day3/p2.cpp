09719501bd1b5eb9a2cb51ec464a2f7bdfb61ece
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-22 08:46:03 +0530

    add two numbers

---
+++ day3/p2.cpp
@@ -0,0 +1,18 @@
+// add two numbers
+
+#include<iostream>
+
+using namespace std;
+
+void printNumber( int num1, int num2)
+{
+ int sum = num1 + num2 ;
+cout << sum << endl;
+}
+
+int main(){
+    int a=8;
+    int b=5;
+    printNumber(a,b);
+    return 0;
+}
