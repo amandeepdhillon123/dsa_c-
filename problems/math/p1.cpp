0e218a5a5d282a91ae252fd141995853aace9894
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-23 08:47:22 +0530

    convert km inot miles

---
+++ problems/math/p1.cpp
@@ -0,0 +1,14 @@
+
+
+//  convert km init miles
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int km;
+   cout << " enter the value of km : ";
+    cin >> km;
+    cout << "Vlaue in miles is" << (1/1.6) * km << endl ;
+
+}
