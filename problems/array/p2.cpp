aaad79a82d25836b0cd6f14cbe44d78b66c9d04f
amandeepdhillon123 <amandeep24397@gmail.com>
2023-03-25 15:33:35 +0530

    array problems

---
+++ problems/array/p2.cpp
@@ -0,0 +1,48 @@
+
+// array  
+
+//  collection of similar datatypes stored in this block
+
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    // int arr[7]; // array declaration
+
+    // char a[106];
+
+    // bool arr[253];
+
+    // int arr[] = {2,3,4,3,5};
+   //  int arr[10] = {2,3,4,3,5};// rest 0
+    //    int arr[3] = {2,3,4,3,5}; // error
+
+    // dynamic array 
+//   insertion 
+/*
+    int arr[4] = {2,3,4,3};
+    char arr[2] = {'a','b'};
+    */
+
+    // access 
+
+     int arr[] = {2,3,4,3};
+
+    //  cout<<arr[3]<< endl;
+
+    for(int i=0;i<4;i++)
+    {
+        cout<<arr[i] << " ";
+    }
+
+
+
+
+
+    
+
+
+    cout << "my name is amandeep "<< endl;
+}
