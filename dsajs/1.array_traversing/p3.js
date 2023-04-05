dea15adef72d5d1bb7b702c7d16fda4e7005174d
amandeepdhillon123 <amandeep24397@gmail.com>
2023-04-05 09:43:00 +0530

    accessing and traverwing element

---
+++ dsajs/1.array_traversing/p3.js
@@ -0,0 +1,15 @@
+ let data=[1,2,3,4,5,6,7,8]
+
+//  console.log(data)
+
+let pos=3;
+// traversing data values 
+for(let i=0; i<data.length;i++)
+{
+    // console.log(data[i])
+
+   
+}
+
+//  accesssing data 
+console.log(data[pos])
