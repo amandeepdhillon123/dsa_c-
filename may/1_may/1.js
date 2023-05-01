1aaf6388f2414db54f2aff694da115f47005309d
amandeepdhillon123 <amandeep24397@gmail.com>
2023-05-01 11:55:28 +0530

    dsa problems

---
+++ may/1_may/1.js
@@ -0,0 +1,21 @@
+
+
+let data=[1,2,3,4,5,6,7,8];
+   
+let pos =4;
+
+// traversing data 
+for(let i=0; i<data.length; i++)
+{
+    // console.log(data[i])
+
+    // accesing
+
+    if(i==pos)
+    {
+        console.log(data[i])
+    }
+}
+
+// accessing data 
+
